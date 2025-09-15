6b69cb4fe9a29430be7ed0d6606926aa9b6d5cb8
qingmei.liu <qingmei.liu@realai.ai>
2025-09-15 18:00:39 +0800

    feat: 配置tailwindcss(v3)

--- src/.umi/testBrowser.tsx
+++ src/.umi/testBrowser.tsx
@@ -3,13 +3,14 @@
 // DO NOT CHANGE IT MANUALLY!
 import React, { useEffect, useState } from 'react';
 import { ApplyPluginsType } from 'umi';
-import { renderClient, RenderClientOpts } from '/Users/realai/Desktop/frontend-demo/node_modules/@umijs/renderer-react';
+import { renderClient, RenderClientOpts } from '/Users/realai/Desktop/frontend-demo/node_modules/.pnpm/@umijs+renderer-react@4.4.12_react-dom@18.3.1_react@18.3.1__react@18.3.1/node_modules/@umijs/renderer-react';
 import { createHistory } from './core/history';
 import { createPluginManager } from './core/plugin';
 import { getRoutes } from './core/route';
 import type { Location } from 'history';
 
 
+import '/Users/realai/Desktop/frontend-demo/src/global.less';
 import 'antd/dist/reset.css';
 const publicPath = '/';
 const runtimePublicPath = false;
