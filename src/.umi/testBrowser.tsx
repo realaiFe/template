0375b74a4cc2522b8aad9646327b92d8df1f4620
qingmei.liu <qingmei.liu@realai.ai>
2025-09-16 17:07:01 +0800

    修改: yarn

--- src/.umi/testBrowser.tsx
+++ src/.umi/testBrowser.tsx
@@ -3,7 +3,7 @@
 // DO NOT CHANGE IT MANUALLY!
 import React, { useEffect, useState } from 'react';
 import { ApplyPluginsType } from 'umi';
-import { renderClient, RenderClientOpts } from '/Users/realai/Desktop/frontend-demo/node_modules/.pnpm/@umijs+renderer-react@4.4.12_react-dom@18.3.1_react@18.3.1__react@18.3.1/node_modules/@umijs/renderer-react';
+import { renderClient, RenderClientOpts } from '/Users/realai/Desktop/frontend-demo/node_modules/@umijs/renderer-react';
 import { createHistory } from './core/history';
 import { createPluginManager } from './core/plugin';
 import { getRoutes } from './core/route';
